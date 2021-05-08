b607ac29d67a3554c8c7669a469242dce879264f
eunjikim <apdlf0708@naver.com>
2021-05-09 02:07:47 +0900

    writcontroller 코딩. 작동안함 에러잡아야함

--- src/main/java/content/WriteActionController.java
+++ src/main/java/content/WriteActionController.java
@@ -1,24 +1,15 @@
 package content;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.UUID;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.google.gson.JsonObject;
 
 //AbstractCommandController =>입력을 받아서 자동적으로 Setter Method 호출
 //public class WriteActionController extends AbstractCommandController {
@@ -49,6 +40,7 @@ public class WriteActionController {
 		int newNum=dao.getNewNum()+1;
 		data.setNum(newNum);//data.getNum() =>#{num}
 
+		System.out.println("WriteActionController의 handle()호출됨");
 
 		String fileName=null;
 		MultipartFile uploadFile = data.getUploadFile();
@@ -57,13 +49,9 @@ public class WriteActionController {
 			String ext = FilenameUtils.getExtension(originalFileName);	//확장자 구하기
 			UUID uuid = UUID.randomUUID();	//UUID 구하기
 			fileName=uuid+"."+ext;
-			uploadFile.transferTo(new File("D:\\upload\\" + fileName));
+			uploadFile.transferTo(new File("C:\\Class\\" + fileName));
 		}
-		vo.setFileName(fileName);
-		boardService.insertBoard(vo);
-
-
-
+			data.setFilename(fileName);
 
 		//수동 => <jsp:setProperty name="객체명" property="*" />
 		data.setTitle(title);
@@ -77,9 +65,6 @@ public class WriteActionController {
 		dao.write(data);//data.getWriter() =>#{writer},,,
 
 		System.out.println("Write content="+content);
-
-
-
 
 		return new ModelAndView("redirect:/list.do");
 
